afeb9df1daa772b579a2b4776adf4e3f99e1c70a
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-10 17:35:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,5 +20,40 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
+  d_index = 0
+  while d_index < nds.length do
+  d_totals = 0
+  result[nds[d_index][:name]]
+  m_index = 0
+  while m_index < nds[d_index][:movies].length do
+    d_totals += nds[d_index][:movies][m_index][:worldwide_gross]
+    m_index += 1
   nil
 end
+  direct_name = nds[d_index][:name]
+  result[direct_name] = d_totals
+end
+d_index += 1
+end
+
+result
+
+end
+=begin d_index = 0
+while d_index < directors_database.length do
+  d_totals = 0
+  result[directors_database[d_index][:name]]
+  m_index = 0
+  while m_index < directors_database[d_index][:movies].length do
+    #puts directors_database[d_index][:movies][m_index][:worldwide_gross]
+    #m_index += 1
+    d_totals += directors_database[d_index][:movies][m_index][:worldwide_gross]
+    m_index += 1
+  end
+direct_name = directors_database[d_index][:name]
+result[direct_name] = d_totals
+#puts d_totals #+= directors_database[d_index][:movies][m_index][:worldwide_gross]  #d_totals += directors_database[d_index][:movies][m_index][:worldwide_gross]
+d_index += 1
+end
+result
+=end

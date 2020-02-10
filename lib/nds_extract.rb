ed3120ec6fb9b4bb9d374d96f63f757aa77f7f36
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-10 17:38:19 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,14 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-
+  m_index = 0
+  director_data = 0
+  gross = 0
+  while m_index < directors_database[director_data][:movies].length do
+    gross += directors_database[director_data][:movies][m_index][:worldwide_gross]
+    m_index += 1
+end
+gross
 end
 
 # Write a method that, given an NDS creates a new Hash
@@ -13,5 +20,39 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-  nil
+  d_index = 0
+  while d_index < nds.length do
+  d_totals = 0
+  result[nds[d_index][:name]]
+  m_index = 0
+  while m_index < nds[d_index][:movies].length do
+    d_totals += nds[d_index][:movies][m_index][:worldwide_gross]
+    m_index += 1
+  #nil
+  direct_name = nds[d_index][:name]
+  result[direct_name] = d_totals
 end
+d_index += 1
+end
+
+result
+
+end
+=begin d_index = 0
+while d_index < directors_database.length do
+  d_totals = 0
+  result[directors_database[d_index][:name]]
+  m_index = 0
+  while m_index < directors_database[d_index][:movies].length do
+    #puts directors_database[d_index][:movies][m_index][:worldwide_gross]
+    #m_index += 1
+    d_totals += directors_database[d_index][:movies][m_index][:worldwide_gross]
+    m_index += 1
+  end
+direct_name = directors_database[d_index][:name]
+result[direct_name] = d_totals
+#puts d_totals #+= directors_database[d_index][:movies][m_index][:worldwide_gross]  #d_totals += directors_database[d_index][:movies][m_index][:worldwide_gross]
+d_index += 1
+end
+result
+=end
